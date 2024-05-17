144aa06e4a96f96aef4ac1dd7d1999c61b45b799
yarkın <iam.xpoxy@gmail.com>
2024-05-17 14:58:09 +0300

    fix issue with index page + adds prefix

--- src/language.js
+++ src/language.js
@@ -1,6 +1,8 @@
-const language_settings_prefix = "--field-";
+const language_settings_data_location = "./src/data/"; // location of language files
+const language_settings_prefix = "--field-"; // prefix before elment ids default: --field-<name>
 
-const document_path = window.location.pathname;
+let document_path = window.location.pathname;
+if(document_path === "/") document_path = "index";
 const page = document_path.split("/").pop().split(".")[0];
 
 const langcode_user_full = window.navigator.userLanguage || window.navigator.language;
@@ -10,14 +12,17 @@ translate(langcode_user);
 
 function translate(langcode) {
     if(!langcode) return;
+    
+    readJson(`${language_settings_data_location}${page}/lang_${langcode}.json`);
 
-    fetch(`./src/data/${page}/lang_${langcode}.json`)
-    .then((response) => response.json())
-    .then((json) => loadElementTranslations(json));
+    function readJson(directory) {
+        fetch(directory)
+        .then((response) => response.json())
+        .then((json) => loadElementTranslations(json));
+    }
 
     function loadElementTranslations(json) {
-        if(!json) return; 
-
+        if(!json) return;
         for(var key in json) {
             document.getElementById(`${language_settings_prefix}${key}`).innerText = json[key];
         }
